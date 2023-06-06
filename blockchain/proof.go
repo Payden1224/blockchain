372c385916ce7cedc6e875a6f6e81f76141d2890
Payden <69829508+Payden1224@users.noreply.github.com>
2023-06-06 07:48:06 -0600

    pow

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -1,3 +1,40 @@
 package blockchain
 
 
+// Take the data from the block
+
+// create a counter (nonce) which starts at 0
+
+// create a hash of the data plus the counter
+
+// check the hash to see if it meets a set of requirements
+
+// Requirements:
+// The First few bytes must contain 0s
+
+const Difficulty = 12 
+
+type ProofOfWork struct {
+	Block *Block
+	Target *big.Int
+}
+
+func NewProof(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-Difficult y))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
+
+func (pow *ProofOfWork) InitData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.Block.PrevHash,
+			pow.Block.Data,
+		},
+		[]byte{},
+	)
+	return data
+}
